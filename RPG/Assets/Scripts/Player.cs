b2d9c53fdb70b44ce368f05304cd6cdeeb24efe7
Paulo Freitas <160852863+paulofreitas19@users.noreply.github.com>
2025-09-08 14:44:17 -0300

    Start programming Player

--- RPG/Assets/Scripts/Player.cs
+++ RPG/Assets/Scripts/Player.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Components")]
+    private Rigidbody2D rig;
+
+    [Header("Stats")]
+    [SerializeField]private int health;
+    [SerializeField]private float speed;
+    [SerializeField]private float initialSpeed;
+    [SerializeField]private float speedRun;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
