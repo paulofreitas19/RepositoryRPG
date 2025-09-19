77b57e50cc4ef32c9d435d3fc7b34ff2413ba40b
Paulo Freitas <160852863+paulofreitas19@users.noreply.github.com>
2025-09-19 15:41:16 -0300

    Programming HUD Gold

--- RPG/Assets/Scripts/HUD/HUDController.cs
+++ RPG/Assets/Scripts/HUD/HUDController.cs
@@ -1,11 +1,14 @@
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class HUDController : MonoBehaviour
 {
     private Player player;
 
     [SerializeField] private Image healthBar;
+    [SerializeField] private Text currentGoldText;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,5 +21,6 @@ public class HUDController : MonoBehaviour
     void Update()
     {
         healthBar.fillAmount = player.Health;
+        currentGoldText.text = "x " + player.CurrentGold.ToString();
     }
 }
